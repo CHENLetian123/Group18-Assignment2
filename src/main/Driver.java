e824ecfb3b20da2dfc90106002b09b57d2e46337
CHENLetian123 <2339512437@qq.com>
2024-05-20 00:02:24 +0800

    Ultimate Edition

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -12,6 +12,43 @@ import utils.DisplayTypeUtility;
 import java.io.File;
 import java.util.List;
 
+
+/*
+Project Task Division Template
+Team leader: CHEN Letian, Student ID: 202383910001
+Team member 1: JIANG Yiwen, Student ID: 202383910006
+Team member 2: TANG Wenting, Student ID: 202383910018
+
+CHEN Letian, Student ID: 202383910001
+I used GitHub Copilot to help me complete some code.
+- Finished most of the Driver.
+- Finished Technology.java.
+- Finished TechnologyDeviceAPI.java.
+- Finished WearableDeviceTest.java, SmartWatchTest.java, SmartBandTest.java with GitHub Copilot.
+- Finished TechnologyDeviceAPITest.java using ChatGPT.
+
+JIANG Yiwen, Student ID: 202383910006
+- Write some of the Driver.
+- Finished ComputingDevice.java.
+- Finished Tablet.java.
+- Finished SmartWatch.java
+- Write all the methods that are called from our menu
+- Write some tests
+
+TANG Wenting, Student ID: 202383910018
+- Write some of the Driver.
+- Finished Manufacturer.java.
+- Finished SmartBand.java
+- Write some tests
+
+
+GitHub Project URL: https://github.com/CHENLetian123/Group18-Assignment2
+
+References are in the References folder.
+
+*/
+
+
 public class Driver {
 
 
